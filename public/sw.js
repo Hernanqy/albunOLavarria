d113ca0f347fe06dc65cbacd7691b1b024153a08
Hernanqy <desarrolladoryotti@gmail.com>
2026-06-28 23:19:40 -0300

    kll

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,13 @@
-﻿const CACHE_NAME = "olavarria-en-figuritas-v1";
-
-const CORE_ASSETS = [
-  "/",
-  "/index.html",
-  "/manifest.webmanifest",
-  "/images/hub/hub-bg.png",
-  "/images/hub/album.png",
-  "/images/hub/scanner-qr.png",
-  "/images/hub/sobre.png",
-  "/images/hub/mapa-ciudad.png",
-  "/images/hub/mis-figuritas.png"
-];
+﻿const CACHE_NAME = "olavarria-en-figuritas-v4";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(CORE_ASSETS);
-    })
-  );
-
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
+      Promise.all(keys.map((key) => caches.delete(key)))
     )
   );
 
@@ -40,20 +18,8 @@ self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) return cachedResponse;
-
-      return fetch(event.request)
-        .then((networkResponse) => {
-          const responseClone = networkResponse.clone();
-
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, responseClone);
-          });
-
-          return networkResponse;
-        })
-        .catch(() => caches.match("/"));
-    })
+    fetch(event.request)
+      .then((response) => response)
+      .catch(() => caches.match(event.request))
   );
 });
